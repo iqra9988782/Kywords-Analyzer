91952845ff38ef2b403578cc6296c928ab55f183
iqra9988782 <iqrabibi9988782@gmail.com>
2024-11-13 12:16:08 +0500

    Create Requirnment.app.py

---
+++ Requirnment.app.py
@@ -0,0 +1,5 @@
+streamlit
+requests       # Optional, for future API integration (e.g., Google Keyword Planner, SEMrush)
+matplotlib     # Optional, for adding graphs/visualization
+beautifulsoup4 # Optional, if you plan to scrape data in the future
+lxml           # Optional, required for BeautifulSoup (web scraping)
